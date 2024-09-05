ee0bbbe54beb1c10080994e0c41d6e2c0c03c1c1
Артём Чижиков <artciz798@gmail.com>
2024-09-05 16:12:03 +0500

    Add files via upload

---
+++ chiiiiiz.py
@@ -0,0 +1,65 @@
+#1
+# print('первое основание(b1) = ')
+# a=int(input())
+# print('второе основание(b2) = ')
+# b= int(input())
+# print('высота(h) =')
+# c= int(input())
+# print('площадь(S) = ', 1/2*(a+b)*c)
+
+#2
+# print('первое cлово - ')
+# a= str(input())
+# print('второе слово - ')
+# b= str(input())
+# print('третье слово) - ')
+# c= str(input())
+# # print(a,b,c)
+
+#3
+# x = int(input())
+# a = x // 100
+# b = x // 10 % 10
+# c = x % 10
+# s = a + b + c
+# print("Сумма цифр =",s)
+# print("Корень =",s**(0.5))
+
+#4
+# print('a = ')
+# a=input()
+# print('b = ')
+# b = input()
+# if a > b:
+#     print('a больше чем b')
+# else:
+#     print('b больше a')
+
+#5
+# print('x = ')
+# x = int(input())
+# if x > 0 or x == 0:
+#     y = 2*x-1
+# elif x < 0:
+#     y=x**2
+# print('y = ', y)
+
+#6
+# print('Пробежал в первый день - ')
+# x=int(input())
+# print('нужно пробежать километров - ')
+# y=int(input())
+# z=1
+# while x < y:
+#     x=x+x*0.1
+#     z=z+1
+# print('Спортсмен пробежит', y, 'километров за', z, 'дней')
+
+#7
+# x,p,y = int(input()),int(input()),int(input())
+# p= p/100
+# z=0
+# while x<y:
+#     x=x+x*p
+#     z=z+1
+# print('За', z, 'лет')
