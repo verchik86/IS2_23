79ad6dc7928a56c4b50aa2acff0ad8f1fc7f5fd4
eegirr <128513975+eegirr@users.noreply.github.com>
2024-09-05 15:39:41 +0500

    Add files via upload

---
+++ zhaoafozd.py
@@ -0,0 +1,24 @@
+# print('первое основание(b1) = ')
+# a=int(input())
+# print('второе основание(b2) = ')
+# b= int(input())
+# print('высота(h) =')
+# c= int(input())
+# print('площадь(S) = ', 1/2*(a+b)*c)
+
+#
+# print('первое cлово - ')
+# a= str(input())
+# print('второе слово - ')
+# b= str(input())
+# print('третье слово) - ')
+# c= str(input())
+# # print(a,b,c)
+
+x = int(input())
+a = x // 100
+b = x // 10 % 10
+c = x % 10
+s = a + b + c
+print("Сумма цифр =",s)
+print("Корень =",s**(0.5))
