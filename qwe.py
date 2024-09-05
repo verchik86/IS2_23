541ee49c87758df09af9c5901ce50d2a13c08edd
kgohjnofrnhlkmteg <166480845+kgohjnofrnhlkmteg@users.noreply.github.com>
2024-09-05 15:41:06 +0500

    Add files via upload

---
+++ qwe.py
@@ -0,0 +1,27 @@
+#print("Введите длину")
+#a = int(input())
+#print("Введите ширину")
+#b = int(input())
+#print("Введите высоту")
+#h = int(input())
+#s = a + b
+#s = s // 2
+#s = s * h
+#print(s)
+
+#print("Введите первое слово")
+#a = str(input())
+#print("Введите второе слово")
+#b = str(input())
+#print("Введите третье слово")
+#c = str(input())
+#print(a, b, c,)
+
+x = int(input())
+a = x // 100
+b = x // 10 % 10
+c = x % 10
+s = a + b + c
+k = s / s
+print("Сумма цифр =",s)
+print("Корень =",s**(0.5))
