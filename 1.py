a844101afb8938937111ef11a2554c96d316572b
xeqqo <nickgoryachev256@gmail.com>
2024-09-05 16:02:57 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+# b1=int(input('введите b1'))
+# b2=int(input('введите b2'))
+# h=int(input('введите h'))1
+# s=1/2*(b1+b2)*h
+# print('s=', s)
+
+# a=str(input("первое слово"))
+# b=str(input("второе слово"))
+# c=str(input("третее слово"))
+# d=a+' '+b+' '+c
+# print(d)
+
+# a= int(input('Введите число x: '))
+# s=sum(int(digit) for digit in str(a))
+# k = s** 0.5
+# print(f's={s}, k={k}')
+
+# a=int(input('первое число'))
+# b=int(input('второе число'))
+# if a>b:
+#     print(f'{a} больше {b}')
+# else:
+#     print(f'{a} меньше {b}')
+
+x=int(input())
+if x
+
+
+
